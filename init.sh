6151073d3b820107b32acf4a568d50575361cd67
Alejandro Perez <alejandro@example.com>
2024-11-29 19:22:17 +0000

    stop.sh modified and create_directories.sh created

--- init.sh
+++ init.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Check if the 'hbase' network exists
-if ! docker network ls | grep -q "hbase"; then
-  echo "Creating 'hbase' network..."
+# Verificar si existe la red 'hbase'
+if ! docker network ls | grep -qw "hbase"; then
+  echo "Creando la red 'hbase'..."
   docker network create hbase
 else
-  echo "'hbase' network already exists."
+  echo "La red 'hbase' ya existe."
 fi
 
-# Now, run docker-compose up
+# Ejecutar el script de creación de directorios
+if [ -f "./create_directories.sh" ]; then
+  echo "Ejecutando create_directories.sh..."
+  sudo ./create_directories.sh
+else
+  echo "El script create_directories.sh no se encontró."
+  exit 1
+fi
+
+# Ejecutar docker-compose
+echo "Iniciando docker-compose..."
 docker-compose up -d --build
